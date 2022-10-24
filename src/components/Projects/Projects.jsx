b995b680b105fbfaa6435c8a7f46860000cc5138
WhosVinicius <silvaviniciusgontijo@gmail.com>
2022-10-24 18:00:28 -0300

    upgrades

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,21 +8,21 @@ function Projects() {
       <div className="icone1">
         {'< />'}
       </div>
-      <div>
-        {projects.map((projeto)=>
+      <div className='card-container'>
+        {projects.map((projeto)=>(
         <a 
         href={projeto.repo}
         key={projeto.titulo}
         target="_blank" rel='noopener noreferrer'
         >
         <div className="card">
-          <img src="" alt="" />
+          <img src= {projeto.img} alt="" />
           <p>{projeto.titulo}</p>
           <div>{projeto.desc}</div>
           <div>{projeto.ref}</div>
         </div>  
         </a>
-        )}
+        ))}
       </div>
     </div>
   )
