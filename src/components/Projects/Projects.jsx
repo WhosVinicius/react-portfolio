1a3cc74c00f431c1987f05e33b833e510fee3aca
WhosVinicius <silvaviniciusgontijo@gmail.com>
2022-10-24 15:55:22 -0300

    2nd push

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import './Projects.css'
-
+import { projects } from './projetos'
 
 function Projects() {
   return (
     <div className='projects-container'>
       <div className="icone1">
         {'< />'}
+      </div>
+      <div>
+        {projects.map((projeto)=>
+        <a 
+        href={projeto.repo}
+        key={projeto.titulo}
+        target="_blank" rel='noopener noreferrer'
+        >
+        <div className="card">
+          teste
+        </div>  
+        </a>
+        )}
       </div>
     </div>
   )
